d3753a71096dc65378f091484352f29368206dab
Alfaz108 <alfazbinrumon@gmail.com>
2024-07-03 10:27:41 +0600

    cros error solve

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './common/exception-filter/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptor/response/response.interceptor';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const httpAdapter = app.get(HttpAdapterHost);
@@ -14,13 +19,6 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials: true,
-  });
-
-  await app.listen(7070);
+  await app.listen(3000);
 }
 bootstrap();

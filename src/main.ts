a3e58d8905540d965f6689e679a73cccc9677f5e
Alfaz108 <alfazbinrumon@gmail.com>
2024-07-04 12:22:30 +0600

    cros problem solve

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,35 @@ import { ResponseInterceptor } from './common/interceptor/response/response.inte
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
   app.enableCors({
     origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
   });
 
+  // Handle preflight requests (OPTIONS)
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
+  // Global pipes
   app.useGlobalPipes(new ValidationPipe());
 
+  // Exception filter
   const httpAdapter = app.get(HttpAdapterHost);
-
   app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
 
+  // Response interceptor
   app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(3000);
